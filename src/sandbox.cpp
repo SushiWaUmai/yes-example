e02426e0794e03c4685ba45de938ad3bfc3c4694
SushiWaUmai <eugene.bb1@gmail.com>
2022-02-27 02:06:37 +0100

    feat: :sparkles: add indexbuffer to vertexarray

--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -21,12 +21,13 @@ void Sandbox::Init()
     };
 
     Ref<VertexArray> vertexArray = VertexArray::Create();
-    Ref<VertexBuffer> vertexBuffer = VertexBuffer::Create(4, ShaderDataType::Vec2, vertices, GL_STATIC_DRAW);
-    Ref<IndexBuffer> indexBuffer = IndexBuffer::Create(2, ShaderDataType::Int3, indices, GL_STATIC_DRAW);
+    Ref<VertexBuffer> vertexBuffer = VertexBuffer::Create(4, ShaderDataType::V2F, vertices, GL_STATIC_DRAW);
+    Ref<IndexBuffer> indexBuffer = IndexBuffer::Create(2, ShaderDataType::V3U, indices, GL_STATIC_DRAW);
 
     Ref<Shader> shader = Shader::Create("./assets/vert.glsl", "./assets/frag.glsl");
 
     vertexArray->AddVertexBuffer(0, vertexBuffer);
+    vertexArray->SetIndexBuffer(indexBuffer);
     vertexArray->EnableAllVertexAttributes();
 
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void *)0);
